03917f052d0aedd0a766be92f09a2ea93e6a5634
Ricardo Corrêa Ribeiro <michels_ribeiro@ig.com.br>
2012-06-16 22:48:12 +0430

    Criada a aplicação
    rails new TrabLpWeb –d sqlite3

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TrabLpWeb::Application.config.secret_token = 'afb33603ff147c35768bf79201a6703f920f4aa1f01ef908b03b15482ea08020597f3519c9690c3d967c0da304b2842a08c660d8fb754a9a722495b1581d6b84'
