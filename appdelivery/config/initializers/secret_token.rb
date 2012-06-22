e1d9224bb726f7fdb70a8e9c6513e1a3a476340c
Vinicius Couto <vc.vine@gmail.com>
2012-06-21 23:43:41 -0300

    Criada a nova aplicação - AppDelivery

---
+++ appdelivery/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Appdelivery::Application.config.secret_token = 'aa207d0dd005e870c7f1d7ae22fd7613b1f5cca08bcf2698e8dd2084348517053f83bf85d6f5639522c665ec568c10b83dbe990cde3fbcf1e634e989048ae134'
