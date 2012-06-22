48b1c962098bd6e117708959b6d9e8665a38e933
Vinicius Couto <vc.vine@gmail.com>
2012-06-21 23:39:34 -0300

    Deletada a aplicacao antiga e movida a pasta da modelagem

--- delivery/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Delivery::Application.config.secret_token = '2f3ada405da4b9508b8203d9f53302bb3d12d81ed38f87b3d4caf5f74e3a9dd08ed0daa3bb211b1556f5ba465fa1633327686af1c85d4ca0e72e4015ad791fad'
